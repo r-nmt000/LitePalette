1f7c777677d0c71e4796eb573d52b41db0b5c30f
Ryusuke Nomoto <r.nmt000@gmail.com>
2019-02-17 20:44:30 +0900

    Add react-native-render-html and marked

--- src/components/SingleNote.js
+++ src/components/SingleNote.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Dimensions, Text, View, ScrollView } from 'react-native';
+import HTML from 'react-native-render-html';
+import marked from 'marked'
 
+const markdown = `
+# 見出し
+ 
+- 箇条書き
+  - 入れ子
+  - 入れ子
+- 箇条書き
+- 箇条書き
+ 
+[Google](https://www.google.co.jp/)
+ 
+> 引用
+ 
+**太字太字**
+ 
+![画像](https://blog.piyo.tech/images/prof.png)
+`;
+
+const htmlContent = marked(markdown);
 
 export default class SingleNote extends React.Component {
   render() {
     return (
-      <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>詳細ページ</Text>
-      </View>
+      <ScrollView style={{ flex: 1 }}>
+        <HTML html={htmlContent} imagesMaxWidth={Dimensions.get('window').width} />
+      </ScrollView>
     );
   }
 }
