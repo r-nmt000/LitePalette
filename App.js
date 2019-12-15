d222bb1a7e73ad5d5e71f82f7cbb64a3acdd1d01
Ryusuke Nomoto <r.nmt000@gmail.com>
2019-12-15 16:58:41 +0900

    Revert redux

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 
 import AppNavigator from './src/navigation/AppNavigator'
 
-
 const AppContainer = createAppContainer(AppNavigator);
-const store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
         <AppContainer />
-      </Provider>
     );
   }
 }
